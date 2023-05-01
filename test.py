76d167c8ce02fa3c063c1df4a647889535840307
Morris_Huang <131897397+morris0927@users.noreply.github.com>
2023-05-01 16:53:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+impot tkinter as tk
 import pandas as pd
 import time
 
@@ -83,4 +84,4 @@ while exitCon==1:
 #print(df)
 df.to_excel('My_PDB.xlsx', index=False)
 
-driver.quit()
+driver.quit()))
