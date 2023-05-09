7890fed30b16ddcf2af4f44066943f850d3d9fe0
morris0927 <131897397+morris0927@users.noreply.github.com>
2023-05-09 20:45:17 +0800

    save

--- pdb.py
+++ pdb.py
@@ -22,7 +22,7 @@ df = pd.DataFrame({'名稱': '', '簡述':'', '序列':'','長度':''}, index=[0
 df.to_excel(writer,index=False,header=False)
 
 # 設定資料庫網址
-url = 'https://www.rcsb.org' 
+url = 'https://www.uniprot.org/' 
 
 #停止登入
 options = webdriver.EdgeOptions()
@@ -38,11 +38,17 @@ wait = WebDriverWait(driver, 20)
 driver.get(url) 
 
 #輸入關鍵字
-search = wait.until( EC.presence_of_element_located((By.ID,"search-bar-input-text")))
+search = wait.until( EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/main/div/div[1]/div/section/form/div[2]/input')))
 search.send_keys(keyWord)
 
 #按下搜尋
-button = wait.until( EC.presence_of_element_located((By.XPATH,'//*[@id="search-icon"]/span')))
+button = wait.until( EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/main/div/div[1]/div/section/form/button')))
+button.click()
+
+button = wait.until( EC.presence_of_element_located((By.XPATH,'/html/body/form/div/span/label[2]/img')))
+button.click()
+
+button = wait.until( EC.presence_of_element_located((By.XPATH,'/html/body/form/div/section/button')))
 button.click()
 
 exitCon = 1
@@ -51,53 +57,48 @@ row = 0
 
 # 開始抓每一個
 while exitCon==1:
+    proteinSeq=""
+    try:
+        title = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/div/main/table/tbody/tr[1]/td[2]/span/a'.format(row+1))))
+        title.click()
+    except:
+        print("title error")
+        break
+    time.sleep(3)
 
-    time.sleep(4)
-    titles = driver.find_elements(By.CLASS_NAME,"results-item")
-    
-    for j in range(len(titles)):
-        try:
-            title = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="app"]/div[3]/div[2]/div[3]/div/div[1]/div[3]/div[3]/div[{}]/div/div[2]/table[1]/tbody/tr/td[1]/h3/a'.format(j+1))))
-            title.click()
-        except:
-            print("title error")
+    #name
+    try:
+        proteinName = driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/h1/span/span').text
+        if titleTemp==proteinName:
             break
-        time.sleep(3)
+        if (j==0):
+            titleTemp = proteinName
+    except:
+        proteinName = "name error"
+    #print(proteinName,end=' ')
+    
 
-        #name
-        try:
-            proteinName = driver.find_element(By.ID,"structureID").text
-            if titleTemp==proteinName:
-                break
-            if (j==0):
-                titleTemp = proteinName
-        except:
-            proteinName = "name error"
-        #print(proteinName,end=' ')
-        
+    #script
+    try:
+        proteinFunc = driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/ul/li[1]/div/div[2]/strong').text
+    except:
+        proteinFunc = "func error"
+    #print(proteinFunc)
 
-        #script
-        try:
-            proteinFunc = driver.find_element(By.ID,"structureTitle").text
-        except:
-            proteinFunc = "func error"
-        #print(proteinFunc)
+    #sequence
+    chunks=driver.find_elements(By.CLASS_NAME,'sequence__chunk')
+    for j in range(len(chunks)):
+        strtemp=driver.find_element(By.XPATH,'//*[@id="sequences"]/div/div[2]/section/div[2]/span[1]').text
+        proteinSeq=proteinSeq+strtemp
 
-        #sequence
-        try:
-            bs = BeautifulSoup(driver.page_source, 'html.parser')
-            proteinSeq = bs.select('g.rcsbElement')[0].text.strip()
-        except:
-            proteinSeq = "Seq error"
-        #print(proteinSeq)
 
-        # 將抓到的資料加入dataframe裡面
-        temp = pd.DataFrame({'名稱':proteinName, '簡述':proteinFunc, '序列':proteinSeq, '長度':len(proteinSeq)}, index= [0])
-        temp.to_excel(writer, index=False, header=False,startrow=row)
-        row+=1
-        print("正在讀取第{}筆資料".format(row))
+    # 將抓到的資料加入dataframe裡面
+    temp = pd.DataFrame({'名稱':proteinName, '簡述':proteinFunc, '序列':proteinSeq, '長度':len(proteinSeq)}, index= [0])
+    temp.to_excel(writer, index=False, header=False,startrow=row)
+    row+=1
+    print("正在讀取第{}筆資料".format(row))
 
-        driver.back()
+    driver.back()
     
     #換頁
     try:
