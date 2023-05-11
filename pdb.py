721e3574be6d7b886117b9a171e2e1f4fd8c4bbb
morris0927 <131897397+morris0927@users.noreply.github.com>
2023-05-11 20:27:57 +0800

    save

--- pdb.py
+++ pdb.py
@@ -1,25 +1,21 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
-import tkinter as tk
 import pandas as pd
 import time
 
 
 #使用者輸入關鍵字
 keyWord = input("請輸入關鍵字: ")
-titleTemp = ""
-proteinName = "1"
+if keyWord=="":
+    keyWord="adhesive"
+    print("default: adhesive")
 writer = pd.ExcelWriter("My_PDB.xlsx",engine="openpyxl")
-
-# 創建一個空的DataFrame來存儲抓取到的數據
-df = pd.DataFrame(index= [0])
-df = pd.DataFrame({'名稱': '', '簡述':'', '序列':'','長度':''}, index=[0])
-
-df.to_excel(writer,index=False,header=False)
 
 # 設定資料庫網址
 url = 'https://www.uniprot.org/' 
@@ -32,7 +28,9 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Edge(options=options)
 driver.get(url=url)
 driver.window_handles
-wait = WebDriverWait(driver, 20)
+driver.maximize_window()
+wait = WebDriverWait(driver, 10)
+action = ActionChains(driver)
 
 # 進入連結
 driver.get(url) 
@@ -51,61 +49,68 @@ button.click()
 button = wait.until( EC.presence_of_element_located((By.XPATH,'/html/body/form/div/section/button')))
 button.click()
 
-exitCon = 1
+rowmax=driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/div[1]/h1/small').text
+rowmax=rowmax.replace(" results","")
+rowmax=rowmax.replace(",","")
+print(rowmax)
 
 row = 0
 
 # 開始抓每一個
-while exitCon==1:
+for i in range(int(rowmax)):
     proteinSeq=""
     try:
-        title = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/div/main/table/tbody/tr[1]/td[2]/span/a'.format(row+1))))
+        title = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div[1]/div/div/main/table/tbody/tr[{}]/td[2]/span/a'.format(row+1))))
+        titlestr=title.text
         title.click()
     except:
+        titlestr="title error"
         print("title error")
-        break
-    time.sleep(3)
+        pass
 
     #name
     try:
-        proteinName = driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/h1/span/span').text
-        if titleTemp==proteinName:
-            break
-        if (j==0):
-            titleTemp = proteinName
+        wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/div/main/h1/span')))
+        proteinName = driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/h1/span').text
     except:
         proteinName = "name error"
-    #print(proteinName,end=' ')
-    
+        pass
+    print(proteinName,end=' ')
 
     #script
     try:
         proteinFunc = driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/ul/li[1]/div/div[2]/strong').text
     except:
         proteinFunc = "func error"
-    #print(proteinFunc)
+        pass
+    print(proteinFunc)
+
+    #滾輪
+    driver.find_element(By.TAG_NAME,'body').send_keys(Keys.END)
+    time.sleep(1)
 
     #sequence
-    chunks=driver.find_elements(By.CLASS_NAME,'sequence__chunk')
+    bs = BeautifulSoup(driver.page_source, 'html.parser')
+    chunks=bs.find_all('span',class_='sequence__chunk')
+    
     for j in range(len(chunks)):
-        strtemp=driver.find_element(By.XPATH,'//*[@id="sequences"]/div/div[2]/section/div[2]/span[1]').text
+        strtemp=driver.find_element(By.XPATH,'//*[@id="sequences"]/div/div[2]/section/div[2]/span[{}]'.format(j+1)).text
         proteinSeq=proteinSeq+strtemp
 
 
     # 將抓到的資料加入dataframe裡面
     temp = pd.DataFrame({'名稱':proteinName, '簡述':proteinFunc, '序列':proteinSeq, '長度':len(proteinSeq)}, index= [0])
     temp.to_excel(writer, index=False, header=False,startrow=row)
-    row+=1
-    print("正在讀取第{}筆資料".format(row))
+    print("正在讀取第{}筆資料".format(row+1))
 
-    driver.back()
-    
-    #換頁
-    try:
-        button = wait.until( EC.presence_of_element_located((By.XPATH,'//*[@title="Step to Next Page"]')))
-        button.click()
-    except:
+    if titlestr!="title error":
+        driver.back()
+    else:
         break
+
+    element=wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="root"]/div[1]/div/div/main/table/tbody/tr[{}]/td[2]/span/a'.format(row+1))))
+    element.location_once_scrolled_into_view
+    row+=1
 
 writer.close()
 #print(df)
